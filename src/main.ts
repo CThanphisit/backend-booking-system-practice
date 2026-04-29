02b618cbe48b1be83bd98b6ba0035af52d1d57b8
Thanphisit(Chern) <thanphisit.p@touchtechnologies.co.th>
2026-04-29 16:03:25 +0700

    fix: CORS main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,8 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
   await app.listen(process.env.PORT ?? 3001);
 }
